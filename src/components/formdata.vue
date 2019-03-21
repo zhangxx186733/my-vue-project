2c13d7bb0d31201dd52eb7b33819236631741199
zhangxx <zhangxiaoxu01@agree.com>
2019-03-21 19:03:12 +0800

    测试

---
+++ src/components/formdata.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input v-model.lazy="msg">
+    <p>{{msg2}}</p>
+  </div>
+</template>
+<script>
+  export default {
+    name:'FormDate',
+    data(){
+      return{
+        msg:'hello',
+        msg2:''
+      }
+    },
+    //方法
+    methods:{
+
+    },
+    //计算属性
+    computed:{
+
+
+    },
+    watch:{
+      msg:function(data){
+          this.msg2=data;
+      }
+    }
+  }
+</script>
+<style></style>
