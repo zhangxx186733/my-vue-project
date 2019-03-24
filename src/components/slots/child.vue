9c533631ccf64ba661a3aa63f2160a922fc672ef
zhangxx <zhangxiaoxu01@agree.com>
2019-03-24 20:27:57 +0800

    插槽学习

---
+++ src/components/slots/child.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="chlid">
+    <hr>
+    <slot></slot>
+    <hr>
+    <slot name="s2"></slot>
+    <hr>
+    <slot name="s3" v-bind:text="msg"></slot>
+    <p></p>
+    <button v-on:click="changeSlot">插槽值改变</button>
+  </div>
+</template>
+<script>
+  export default {
+    name:'child',
+    data(){
+      return{
+        msg:"作用域插槽124214"
+      }
+    },
+    methods:{
+      changeSlot(){
+        this.msg="5656";
+      }
+    }
+  }
+</script>
+<style></style>
---
+++ src/components/slots/parent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    父模块
+    <p></p>
+    <Child>
+      <!--普通插槽的写法-->
+      <p>普通插槽</p>
+      <!--具名插槽的写法-->
+      <p slot="s2">具名插槽</p>
+      <!--作用域插槽的写法-->
+      <p slot="s3" slot-scope="key">
+        {{key.text}}
+      </p>
+    </Child>
+  </div>
+</template>
+<script>
+  import Child from './child'
+  export default {
+    name:'parent',
+    data(){
+      return{
+
+      }
+    },
+    components:{
+      Child
+    }
+  }
+</script>
+<style></style>
