bda35c9706cd6f32baad1356d6a8a22bdda82ec9
zhangxx <zhangxiaoxu01@agree.com>
2019-03-24 23:57:56 +0800

    自定义组件

---
+++ src/components/defauleCom/defaultCom.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!--默认指定焦点-->
+    <input v-focus>
+    <p v-mycss>自定义组件</p>
+  </div>
+</template>
+<script>
+  export default {
+    name:'Default',
+    data(){
+      return{}
+    },
+    //定义一个mycss的组件
+    directives:{
+        mycss:function (el) {
+          el.style.color="red";
+        }
+    }
+  }
+</script>
+<style></style>
